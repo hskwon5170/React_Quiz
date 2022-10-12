116234cf0c6413678e2ca73154e9eb9ee91dc2ed
hskwon5170 <hskwon517@gmail.com>
2022-10-12 14:48:13 +0900

    [test] quizDetail 테스트코드 작성

--- cypress/e2e/check/index.cy.ts
+++ cypress/e2e/check/index.cy.ts
@@ -3,4 +3,13 @@ it("퀴즈페이지로 이동", () => {
   cy.get("div").click({ multiple: true });
   cy.get("section");
 });
+it("퀴즈디테일page", () => {
+  cy.visit(`http://localhost:3000/quiz/detail/25}`);
+  cy.get("button").click();
+  cy.get("div");
+  cy.get("section");
+  cy.get("div");
+  cy.get("h3");
+  cy.get("pre");
+});
 export {};
